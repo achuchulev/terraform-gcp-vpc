d6ba344a41b3403a2ad8bef62274769a914a0c46
Atanas Chuchulev <atanas.v4@gmail.com>
2023-06-27 15:37:34 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = file(var.gcp_credentials_file_path)
+  #credentials = file(var.gcp_credentials_file_path)
 
   # Should be able to parse project from credentials file but cannot.
   # Cannot convert string to map and cannot interpolate within variables.
